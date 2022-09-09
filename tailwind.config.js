3142722d46582fa4977325848df46079a1548b9b
Maisam Afshar <maisam.afshar@gmail.com>
2022-09-09 17:28:57 +0430

    add custom font-poppins & gray color

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,21 @@ module.exports = withMT({
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
-  ],  theme: {
+    "./sections/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      poppins: "Poppins"
+    },
+    colors: {
+      gray: {
+        50: "#fafafa",
+        100: "#f5f5f5",
+        200: "#eeeeee",
+        300: "#e0e0e0",
+        400: "#bdbdbd",
+      }
+    },
     extend: {},
   },
   plugins: [],
