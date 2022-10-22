2abd4870db2d2b105f07895feea544e40885e3b6
Maisam Afshar <maisam.afshar@gmail.com>
2022-10-22 20:15:25 +0430

    remove made by section

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -225,15 +225,15 @@ const Index = () => {
           <Link href="#">Privacy policy</Link>
         </div>
       </div>
-      <Typography
-        variant="paragraph"
-        className="absolute left-0 right-0 text-sm text-center text-gray-200 bottom-2 font-poppins"
-      >
-        Made with ❤️ by{" "}
-        <a href="https://twitter.com/imaisam" target="_blank" rel="noreferrer">
-          @imaisam
-        </a>
-      </Typography>
+//       <Typography
+//         variant="paragraph"
+//         className="absolute left-0 right-0 text-sm text-center text-gray-200 bottom-2 font-poppins"
+//       >
+//         Made with ❤️ by{" "}
+//         <a href="https://twitter.com/imaisam" target="_blank" rel="noreferrer">
+//           @imaisam
+//         </a>
+//       </Typography>
     </footer>
   );
 };
